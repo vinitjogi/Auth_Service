88bd22cf9cbf9ae031f2e64e593164be41f9af6c
vinit jogi <vinit.jogi@somaiya.edu>
2023-10-05 19:47:37 +0530

    added email & password validation for incomimg request

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const { AuthRequestValidator } = require('../../middleware/index');
 const userController = require('../../controllers/user-controller');
 
 const router = express.Router();
 
-router.post('/signup', userController.create);
-router.post('/signin', userController.signIn);
+router.post(
+    '/signup', 
+    AuthRequestValidator.validateUserAuth, 
+    userController.create
+);
+router.post(
+    '/signin', 
+    AuthRequestValidator.validateUserAuth, 
+    userController.signIn
+);
 
 
 module.exports = router;
